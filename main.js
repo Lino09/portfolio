483d6968eac6daa2f1658702383f7d53a14f3fee
Lino09 <onosmx@gmail.com>
2021-12-28 21:42:44 -0600

    Add form functionality

--- main.js
+++ main.js
@@ -98,3 +98,15 @@ window.addEventListener('scroll',() => moveCamera());
 
 
 animate();
+
+//form section 
+
+const form = document.querySelector('#contact-form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+    form.submit();
+    document.querySelector('#name').value = '';
+    document.querySelector('#email').value = '';
+    document.querySelector('#message').value = '';
+});
